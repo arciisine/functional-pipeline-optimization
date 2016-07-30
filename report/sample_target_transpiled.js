91dcbed3ca176adb22f3ef759830055671183ce9
Timothy Soehnlin <tim@eaiti.com>
2016-07-30 16:02:05 -0400

    Working on outline

--- report/sample_target_transpiled.js
+++ report/sample_target_transpiled.js
@@ -1,6 +1,6 @@
 let sumAndCount = EXEC(WRAP(findPeople())
-  .filter(TAG(p => p.age > 35 && p.gender == 'male'))
-  .map(TAG(p => p.name))
-  .reduce(TAG((acc, p) => [acc[0] + p.income, acc[1] + 1], [0,0])))
+  .filter(TAG(p => p.age > 35 && p.gender == 'male'), UNIQUE_ID_1, true)
+  .map(TAG(p => p.name), UNIQUE_ID_2, true)
+  .reduce(TAG((acc, p) => [acc[0] + p.income, acc[1] + 1], [0,0]), UNIQUE_ID_3, true))
 
 let avgIncome = sumAndCount[0]/sumAndCount[1];
