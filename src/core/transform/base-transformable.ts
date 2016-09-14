4f80e2283a3eac941ed98a20268a511adf3dfe9d
Timothy Soehnlin <tim@eaiti.com>
2016-09-14 13:55:14 -0500

    Final reference to node_modules

--- src/core/transform/base-transformable.ts
+++ src/core/transform/base-transformable.ts
@@ -1,4 +1,4 @@
-import { AST, Macro as m } from '../../../node_modules/@arcsine/ecma-ast-transform/src';
+import { AST, Macro as m } from '@arcsine/ecma-ast-transform/src';
 import { Transformable, TransformResponse } from '../../core';
 
 export abstract class BaseTransformable<I, O> implements Transformable<I, O>  {
