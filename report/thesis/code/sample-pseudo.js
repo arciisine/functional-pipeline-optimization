9ebd28e6f5ebe4ed3c8133d4fe345c8a1c3d3365
Timothy Soehnlin <tim@eaiti.com>
2016-09-26 17:09:00 -0500

    Adding in review feedback

--- report/thesis/code/sample-pseudo.js
+++ report/thesis/code/sample-pseudo.js
@@ -1,7 +1,7 @@
 function findCommonWords(text, threshold) {
   // Split text by non alphanumeric characters
   // For word in words
-  //    Skip if the word length is less than 3 (ignore small words)
+  //    Skip if the word length is less than three (ignore small words)
   //    Convert the word to lower case
   //    Increment counter for word
   //    If counter passes threshold
