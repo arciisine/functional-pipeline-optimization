297a8554ad708d41ebc5d0f2a0f29f669b2b89a4
Timothy Soehnlin <tim@eaiti.com>
2016-09-25 23:00:51 -0500

    Cleaning up code reference

--- report/thesis/code/simple-functional-final.js
+++ report/thesis/code/simple-functional-final.js
@@ -7,28 +7,26 @@ function simple(specialFilter) {
   EXEC(
     [1,2,3]/* target*/,
     '__key_RANDOM_UUID', /* callsite id */,
-    [  /* Chain operations  */
-      'map',
-      'filter',
-      'map',
-      'filter'
-    ],
+    __operators__UUID,
     [ /* Chain inputs */
-      [function __inline_1(x) { return  x * 2 }],
-      [function __inline_2(x) { return x > __thisId.limit }]
-      [function __inline_3(x,i) {
+      [function __uuid1(x) { return  x * 2 }],
+      [function __uuid2(x) { return x > __thisId.limit }]
+      [function __uuid3(x,i) {
         count += 1
         return Math.pow(x, i+1)
       }],
       [specialFilter]
     ], 
-    [ /* Indicate if any chain functions were passed in as variables */
-      false,
-      false,
-      false,
-      true
-    ],
     [__thisId, count], /* List of all closed variables */
     (__randomId) => { count = __randomId } /* Closure reassignment*/
   )
 }
+
+
+//Operators and flag to indicate inline vs static vs dynamic
+var __operators__UUID = [
+  ['map', 0],
+  ['filter', 0],
+  ['map', 0],
+  ['filter', 1]
+]
